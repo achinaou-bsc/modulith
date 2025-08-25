29bae251f0ee90c7f9bb16fc3b9f456c75b6e649
Achilleas Naoumidis <achinaou@gmail.com>
2025-08-25 16:35:40 +0200

    Remove the delete button and upgrade job versions

--- modules/application/src/dev/a4i/bsc/modulith/application/view/JobTableRow.scala
+++ modules/application/src/dev/a4i/bsc/modulith/application/view/JobTableRow.scala
@@ -54,19 +54,11 @@ object JobTableRow:
       td(
         classAttr := "px-6 py-4",
         button(
-          typeAttr      := "button",
-          hxGetAttr     := s"/jobs/viewer/${model.id}",
-          hxTargetAttr  := "#main",
-          classAttr     := "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700",
+          typeAttr     := "button",
+          hxGetAttr    := s"/jobs/viewer/${model.id}",
+          hxTargetAttr := "#main",
+          classAttr    := "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700",
           "View"
-        ),
-        button(
-          typeAttr      := "button",
-          hxDeleteAttr  := s"/jobs/table/${model.id}",
-          hxTargetAttr  := "#main",
-          hxConfirmAttr := s"Delete job ${model.id}?",
-          classAttr     := "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
-          "Delete"
         )
       )
     )
